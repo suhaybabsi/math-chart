57367dbee2e34c0cab3736a63bc3f572bac6d3e3
suhaybabsi <suhaybal-absi@192.168.1.15>
2017-12-01 22:26:37 +0200

    update home

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use('/app', express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/app'));
 
 app.get('/', function (request, response) {
   res.render('404', { url: req.url });
