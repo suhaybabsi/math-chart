f63f4fd84c30c0497ebfd1aff9a9faf4461277ed
suhaybabsi <suhaybal-absi@192.168.1.10>
2018-08-13 23:44:01 +0300

    add, subtract

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function (request, response) {
   res.render('404', { url: req.url });
 });
 
-app.get('/sum', function (request, response) {
+app.get('/add', function (request, response) {
 
   var q = request.query;
 
@@ -29,6 +29,29 @@ app.get('/multiply', function (request, response) {
 
   response.send("Result: " + result);
 });
+
+app.get('/divide', function (request, response) {
+
+  var q = request.query;
+
+  let no1 = q.no1;
+  let no2 = q.no2;
+  let result = Number(no1) / Number(no2);
+
+  response.send("Result: " + result);
+});
+
+app.get('/subtract', function (request, response) {
+
+  var q = request.query;
+
+  let no1 = q.no1;
+  let no2 = q.no2;
+  let result = Number(no1) - Number(no2);
+
+  response.send("Result: " + result);
+});
+
 
 app.listen(app.get('port'), function () {
   console.log('Math-chart is running on port', app.get('port'));
